6f685058ceed87775fa34e1871a22c776766e08e
Wawan Kurniawan <ones07@gmail.com>
2012-02-22 21:31:28 +0700

    assets re-compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rollingthemes::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['maintenance/*.js', 'maintenance/*.css', 'ckeditor/*.js', 'web_app_theme.css']
+  config.assets.precompile += ['maintenance/*.js', 'maintenance/*.css', 'ckeditor/*.js', 'ckeditor/*.css', 'web_app_theme.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
